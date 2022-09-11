55ee888246485a940506f03dbbc8ca44f64e9965
Muhammad A Faishal <maafaishal@gmail.com>
2022-09-11 19:26:12 +0700

    chore: dummy

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { signOut, useSession } from "next-auth/react";
 
 const Header: React.FC = () => {
   const router = useRouter();
+
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
 
